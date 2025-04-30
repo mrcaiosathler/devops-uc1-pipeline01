c8e5069d7274b4aefa6b1db300e9975f0930d012
Caio Sathler de Lima <sathler.caio@gmail.com>
2025-04-29 21:33:49 -0300

    Fix no código gerador de gráficos

--- gerar_grafico.py
+++ gerar_grafico.py
@@ -1,7 +1,18 @@
 import pandas as pd  
 import matplotlib.pyplot as plt  
 
-df = pd.read_csv('data.csv')  
-df['idade'].plot(kind='hist')  
-plt.title('Distribuição de Idades')  
-plt.savefig('grafico.png')
+df = pd.read_csv('data.csv') 
+
+#Cria Figura e Eixos explicitamente
+fig, ax = plt.subplots()
+
+#Associa o plot aos eixos criados
+df['idade'].plot(kind='hist', ax=ax)
+ax.set_title('Distribuição de Idades')  
+
+#Salva Figura
+fig.savefig('grafico.png')
+
+#Fecha a figura para liberar memória
+plt.close()
+
